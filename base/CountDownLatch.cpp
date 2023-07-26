041fd5f1ab705693ae1badfdd9498c44ddf95785
faliks <243650719@qq.com>
2023-07-26 15:34:47 +0800

    add CountDownLatch class to async different thread.

---
+++ base/CountDownLatch.cpp
@@ -0,0 +1,26 @@
+#include "base/include/CountDownLatch.h"
+
+
+faliks::CountDownLatch::CountDownLatch(int count)
+        : m_mutex(),
+          m_count(count),
+          m_condition() {
+}
+
+void faliks::CountDownLatch::wait() {
+    std::unique_lock<std::mutex> lock(m_mutex);
+    m_condition.wait(lock, [this] { return m_count == 0; });
+}
+
+void faliks::CountDownLatch::countDown() {
+    std::unique_lock<std::mutex> lock(m_mutex);
+    --m_count;
+    if (m_count == 0) {
+        m_condition.notify_all();
+    }
+}
+
+int faliks::CountDownLatch::getCount() const {
+    std::scoped_lock<std::mutex> lock(m_mutex);
+    return m_count;
+}
---
+++ base/include/CountDownLatch.h
@@ -0,0 +1,28 @@
+#ifndef MUDUO_LEARN_COUNTDOWNLATCH_H
+#define MUDUO_LEARN_COUNTDOWNLATCH_H
+
+#include "base/include/NoneCopyable.h"
+#include "base/include/ThreadSaftyCheck.h"
+
+#include <mutex>
+#include <condition_variable>
+
+namespace faliks {
+    class CountDownLatch : public NoneCopyable {
+    private:
+        std::mutex m_mutex;
+        int m_count THREAD_ANNOTATION_ATTRIBUTE__(guarded_by(m_mutex));
+        std::condition_variable m_condition;
+    public:
+        explicit CountDownLatch(int count);
+
+        void wait();
+
+        void countDown();
+
+        int getCount() const;
+    };
+}
+
+
+#endif //MUDUO_LEARN_COUNTDOWNLATCH_H
